865862d465b8af08bfdee4c8654fa2e9fed65df5
David Egbert <dmegbert@gmail.com>
2020-09-23 21:18:31 -0400

    Switch to pure-react-carousel

--- gb-ui/src/components/GbCarousel.js
+++ gb-ui/src/components/GbCarousel.js
@@ -1,31 +1,34 @@
-import React from "react"
-import { Carousel } from "react-responsive-carousel"
+import React from "react";
+import {CarouselProvider, Image, Slide, Slider} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
 
-import evergreen1 from "../imgs/evergreen-home.png"
-import evergreen2 from "../imgs/evergreen-protoype.jpg"
-import evergreen3 from "../imgs/evergreen-dashboard.png"
-
-// general styles
-import 'style!css!react-responsive-carousel/lib/styles/main.css';
-
-// carousel styles
-import 'style!css!react-responsive-carousel/lib/styles/carousel.css';
+import evergreen1 from "../imgs/evergreen-home.png";
+import evergreen2 from "../imgs/evergreen-protoype.jpg";
+import evergreen3 from "../imgs/evergreen-dashboard.png";
 
 const GbCarousel = () => {
-
-	return (
-		<Carousel showThumbs={false} swipeable={true} autoPlay={true} emulateTouch={true} infiniteLoop={true} stopOnHover={true} showStatus={false}>
-			<div>
-				<img src={evergreen1} alt="evergreen home"/>
-			</div>
-			<div>
-				<img src={evergreen2} alt="evergreen prototype"/>
-			</div>
-			<div>
-				<img src={evergreen3} alt="evergreen dashboard"/>
-			</div>
-		</Carousel>
-	)
-}
+  return (
+    <CarouselProvider
+      naturalSlideWidth={1200}
+      naturalSlideHeight={800}
+      totalSlides={3}
+      interval={3000}
+      isPlaying={true}
+      infinite={true}
+    >
+      <Slider>
+        <Slide index={0}>
+          <Image alt="evergreen home page" src={evergreen1}/>
+        </Slide>
+        <Slide index={1}>
+          <Image alt="evergreen prototype" src={evergreen2}/>
+        </Slide>
+        <Slide index={2}>
+          <Image alt="evergreen dashboard" src={evergreen3}/>
+        </Slide>
+      </Slider>
+    </CarouselProvider>
+  );
+};
 
 export default GbCarousel
